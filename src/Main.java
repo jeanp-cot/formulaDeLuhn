0d53de687f2c6b1a0f3f3bc76cf670157fc89f9e
J34nPC24 <jean.cotera@epn.edu.ec>
2022-09-30 13:00:57 -0500

    Trabajo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String numeroDeTarjeta = "4539319503436467";
+
+
+        if (numeroDeTarjetaEsValidoModuloDiez(numeroDeTarjeta)) {
+            System.out.println("Numero valido");
+        } else {
+            System.out.println("Numero invalido");
+        }
+
+        String numeroDeTarjetaValidoParaModulo2 = "4739319503436467";
+
+        if (numeroDeTarjetaEsValidoModuloN(numeroDeTarjetaValidoParaModulo2, 2)) {
+            System.out.println("Numero valido");
+        } else {
+            System.out.println("Numero invalido");
+        }
+    }
+
+    private static boolean numeroDeTarjetaEsValidoModuloN(String numerosDeTarjeta, int modulo) {
+        //Separo cada digito en un espacio del arreglo
+        int[] numerosDeTarjetaSeparados = new int[numerosDeTarjeta.length()];
+
+        int auxiliarParaIndice = 0;
+
+        //Los guardo iniciando desde la derecha para que me sea mas facil conseguir cada segundo digito
+
+        for (int i= numerosDeTarjeta.length()-1 ; i>=0 ; i--){
+            numerosDeTarjetaSeparados[auxiliarParaIndice] = Character.getNumericValue(numerosDeTarjeta.charAt(i));
+            auxiliarParaIndice++;
+        }
+
+        //Aplico el algoritmo
+        for (int i=0; i<numerosDeTarjetaSeparados.length;i++){
+            if (i % 2 != 0) {
+                numerosDeTarjetaSeparados[i] *= 2;
+                if (numerosDeTarjetaSeparados[i] > 9) {
+                    numerosDeTarjetaSeparados[i] -= 9;
+                }
+            }
+        }
+
+        int sumaTotal = 0;
+
+        for (int numeroDeTarjeta : numerosDeTarjetaSeparados) {
+            sumaTotal += numeroDeTarjeta;
+        }
+
+        System.out.println(Arrays.toString(numerosDeTarjetaSeparados) + sumaTotal);
+
+        return sumaTotal % modulo == 0;
+    }
+
+    private static boolean numeroDeTarjetaEsValidoModuloDiez(String numerosDeTarjeta) {
+        return numeroDeTarjetaEsValidoModuloN(numerosDeTarjeta, 10);
     }
 }
